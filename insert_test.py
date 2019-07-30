1162751f9e19a4ad262c8cafc4b931b7b989925e
bAcheron <43649159+bAcheron@users.noreply.github.com>
2019-07-29 21:38:54 -0700

    Update insert_test.py

--- insert_test.py
+++ insert_test.py
@@ -11,17 +11,17 @@ log_name='logs\\'+current_date +'_test_etl.log'
 test_db = NewDatabaseManager('DESKTOP-QP2G51N\SQLEXPRESS','Examples',log_name)
 
 file_list = test_db.select_data("""
-								SELECT 
-										f.File_Location
-										,f.File_type
-										,f.File_Name
-										,table_name
-									  ,e.Extract_File_Location
-								  FROM [Examples].[dbo].[Meta_Raw_File_Extracts] e
-								  Join [dbo].[Meta_Raw_Files] f
-								  on e.file_type = f.file_type
-								  and e.file_provider = f.file_provider
-								  where is_loaded = 0""")
+		SELECT 
+			f.File_Location
+			,f.File_type
+			,f.File_Name
+			,table_name
+			,e.Extract_File_Location
+		FROM [Examples].[dbo].[Meta_Raw_File_Extracts] e
+		Join [dbo].[Meta_Raw_Files] f
+		  on e.file_type = f.file_type
+		  and e.file_provider = f.file_provider
+		  where is_loaded = 0""")
 for file in file_list:
 	with open(file[0],'r') as f:
 		reader = csv.reader(f)
